e05ba4d72a2632bb4996f334968952b5dc4eee4c
Wujunxian <wu812730157@gmail.com>
2018-01-30 09:41:34 +0800

    added CatalogBrowser Filter functions

--- app/components/HomePage/CatalogBrowser.jsx
+++ app/components/HomePage/CatalogBrowser.jsx
@@ -4,13 +4,132 @@
  Description:
  */
 import React from 'react';
+import {Button, FormControl, FormGroup, InputGroup} from "react-bootstrap";
+import {RestClient, SERVER} from "../Util/RestClient";
+import {convertToItems, doFilter} from "./CatalogBrowser.run";
 
 class CatalogBrowser extends React.Component {
+    constructor() {
+        super();
+        this.handleSearchCatalog = this.handleSearchCatalog.bind(this);
+        this.onTemplateSelected = this.onTemplateSelected.bind(this);
+
+        this.state = {templates: [], imageStreams: []};
+    }
+
+    onTemplateSelected(template) {
+        console.log("template selected");
+        console.log(template);
+    }
+
+    handleSearchCatalog() {
+        let keyword = $('#inputSearchCatalog').val();
+        console.log(keyword);
+    }
+
+    componentDidMount() {
+        let templateSuccess = function (data) {
+            this.setState({templates: data.items});
+        };
+        let imageStreamSuccess = function (data) {
+            this.setState({imageStreams: data.items});
+        };
+
+        let error = function (data, status) {
+        };
+
+        RestClient.get(SERVER.MOCK, "/server/templates.json", undefined, templateSuccess, error, undefined, this);
+        RestClient.get(SERVER.MOCK, "/server/imageStreams.json", undefined, imageStreamSuccess, error, undefined, this);
+    }
+
     render() {
-        return (<div/>
-            /* Insert your Component Here */
+        return (
+            <div className="content-wrapper">
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl id="isunputSearchCatalog" type="text" placeholder={"搜索模板库..."}/>
+                        <InputGroup.Button>
+                            <Button title="搜索" onClick={this.handleSearchCatalog}><em
+                                className="fa fa-search"/></Button>
+                        </InputGroup.Button>
+                    </InputGroup>
+                </FormGroup>
+
+                {/*Start Browser Panel*/}
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h4>浏览模板库
+                            <small className="pull-right">
+                                <a>部署镜像</a>
+                                &nbsp;
+                                <a>导入YAML/JSON</a>
+                                &nbsp;
+                                <a>从项目中选择</a>
+                            </small></h4>
+                    </div>
+                    <div className="panel-body">
+                        <TemplateDisplay templates={this.state.templates} imageStreams={this.state.imageStreams}
+                                         onTemplateSelected={this.onTemplateSelected}/>
+                    </div>
+                </div>
+            </div>
         );
     }
+}
+
+class TemplateDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onTemplateSelected = props.onTemplateSelected;
+    }
+
+    handleTemplateClick() {
+
+    }
+
+    render() {
+        let result = convertToItems(this.props.templates, this.props.imageStreams);
+        console.log(result);
+
+        return (
+            <div>{JSON.stringify(result)}</div>
+        );
+    }
+}
+
+class TemplateFilterPanel extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let filter = {
+            keyword: props.keyword, primaryType: props.primaryType,
+            secondaryType: props.secondaryType
+        };
+
+        this.state.collapse = true;
+        this.props.filteredItems = doFilter(this.props.items, filter);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+
+    }
+}
+
+export class Item {
+    iconClass;
+    displayName;
+    primaryType;
+    secondaryType;
+    data;
+}
+
+export class Filter {
+    keyword;
+    primaryType;
+    secondaryType;
 }
 
 export default CatalogBrowser;
---
+++ app/components/HomePage/CatalogBrowser.run.js
@@ -0,0 +1,159 @@
+import {Filter, Item} from "./CatalogBrowser";
+
+/**
+ * tag-primaryType conversion map
+ * @type {{java: string}}
+ */
+const Category = {
+    'language':
+        {
+            'java': {iconUrl: 'logo/java.svg', tags: ['java']},
+            'dotnet': {iconUrl: 'logo/dotnet.svg', tags: ['dotnet', '.net']},
+            'javascript': {iconUrl: 'logo/js.svg', tags: ['nodejs']},
+            'perl': {iconUrl: 'logo/perl.svg', tags: ['perl']},
+            'ruby': {iconUrl: 'logo/ruby.svg', tags: ['ruby']},
+            'php': {iconUrl: 'logo/php.svg', tags: ['php']},
+            'python': {iconUrl: 'logo/python.svg'}
+        },
+    'database':
+        {
+            'mongo': {iconUrl: 'logo/mongodb.svg', tags: ['mongodb']},
+            'mysql': {iconUrl: 'logo/mysql-database.svg', tags: ['mysql']},
+            'postgres': {iconUrl: 'logo/postgresql.svg', tags: ['postgresql']},
+            'mariadb': {iconUrl: 'logo/mariadb.svg', tags: ['mariadb']},
+        },
+    'middleware': {
+        'runtime': {tags: ['httpd']}
+    },
+    'cicd': {
+        'jenkins': {iconUrl: 'logo/jenkins.svg', tags: ['jenkins']}
+    }
+};
+
+let TagMap = (function () {
+    let result = {};
+
+    for (let primaryType in Category) {
+        for (let secondaryType in Category[primaryType]) {
+            if (Category[primaryType].hasOwnProperty(secondaryType)) {
+                for (let i = 0; i < Category[primaryType][secondaryType].tags.length; i++) {
+                    result[Category[primaryType][secondaryType].tags[i]] = {primaryType, secondaryType};
+                }
+            }
+        }
+    }
+
+    return result;
+})();
+
+
+/**
+ *
+ * @param {array} templates
+ * @param {array} imageStreams
+ */
+function convertToItems(templates, imageStreams) {
+    let result = [];
+
+    for (let i = 0; i < templates.length; i++) {
+        result.push(convertTemplateToItem(templates[i]));
+    }
+
+    for (let i = 0; i < imageStreams.length; i++) {
+        result.push(convertImageStreamToItem(imageStreams[i]));
+    }
+
+    return result;
+}
+
+function convertTemplateToItem(template) {
+    let result = new Item();
+    let annotations = template.metadata.annotations;
+    if (annotations) {
+        result.iconClass = annotations.iconClass;
+        result.displayName = annotations['openshift.io/display-name'] || template.metadata.name;
+        if (annotations.tags) {
+            findTag(annotations, result);
+        }
+    } else {
+        result.displayName = template.metadata.name;
+    }
+    result.data = template;
+    return result;
+}
+
+/**
+ *
+ * @param annotations
+ * @param {Item} item
+ */
+function findTag(annotations, item) {
+    let thisTags = annotations.tags.split(",");
+    for (let i = 0; i < thisTags.length; i++) {
+        if (TagMap[thisTags]) {
+            item.primaryType = TagMap[thisTags].primaryType;
+            item.secondaryType = TagMap[thisTags].secondaryType;
+            break;
+        }
+    }
+}
+
+function convertImageStreamToItem(imageStream) {
+    let result = new Item();
+    result.data = imageStream;
+    result.displayName = imageStream.metadata.name;
+
+    let annotations = imageStream.metadata.annotations;
+    if (annotations) {
+        result.displayName = annotations['openshift.io/display-name'];
+    }
+
+    let tag = imageStream.spec.tags[0];
+    annotations = tag.annotations;
+    if (tag) {
+        if (annotations) {
+            result.iconClass = annotations.iconClass;
+            if (annotations.tags) {
+                findTag(annotations, result);
+            }
+        }
+    }
+
+    return result;
+}
+
+/**
+ *
+ * @param {Item[]} items
+ * @param {Filter} filter
+ */
+function doFilter(items, filter) {
+    //define filter function, each filter one property
+    let filters = [];
+    if (typeof filter.keyword !== 'undefined')
+        filters.push((item) =>
+            item.displayName.includes(filter.keyword));
+    if (typeof filter.primaryType !== 'undefined')
+        filters.push((item) =>
+            item.primaryType === filter.primaryType);
+    if (typeof filter.secondaryType !== 'undefined')
+        filters.push((item) =>
+            item.secondaryType === filter.secondaryType);
+
+    let result = [];
+    for (let i = 0; i < items.length; i++) {
+        let filtered = false;
+        for (let j = 0; j < filters.length; j++) {
+            if (!filters[j](items[i])) {
+                filtered = true;
+                break;
+            }
+        }
+        if (!filtered)
+            result.push(items[i]);
+    }
+
+    return result;
+}
+
+export {convertToItems, doFilter}
