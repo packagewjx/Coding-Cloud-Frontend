efd1c65df90bc878b6c8126f2983de773ba90258
Wujunxian <wu812730157@gmail.com>
2018-01-31 14:40:49 +0800

    added item view, now we can see the item name and icon.

--- app/components/HomePage/CatalogBrowser.jsx
+++ app/components/HomePage/CatalogBrowser.jsx
@@ -1,13 +1,15 @@
 /**
  Create Date: 1/23/18
  @author <a href="mailto:wu812730157@gmail.com">Wujunxian</a>
- Description:
+ Description: This is the component used to display all public templates and imageStreams (of namespace openshift), it
+ provide filter and two-level menu to select the item.
  */
 import React from 'react';
 import {Button, Col, Collapse, FormControl, FormGroup, Image, InputGroup, Row, Tab, Tabs} from "react-bootstrap";
 import {RestClient, SERVER} from "../Util/RestClient";
-import {Category, convertToItems, doFilter} from "./CatalogBrowser.run";
+import {convertToItems, doFilter} from "./CatalogBrowser.run";
 import {getAbsoluteOffsetTop} from "../Util/StylingUtils";
+import {Category, findIconClass} from "./constants";
 
 class CatalogBrowser extends React.Component {
     constructor() {
@@ -18,6 +20,7 @@ class CatalogBrowser extends React.Component {
         this.onThumbnailClick = this.onThumbnailClick.bind(this);
         this.changeFilter = this.changeFilter.bind(this);
         this.togglePanelOpen = this.togglePanelOpen.bind(this);
+        this.onItemClick = this.onItemClick.bind(this);
 
         this.state = {templates: [], imageStreams: [], filter: new Filter(), open: true};
 
@@ -66,6 +69,14 @@ class CatalogBrowser extends React.Component {
         }
     }
 
+    /**
+     *
+     * @param {Item} item
+     **/
+    onItemClick(item) {
+
+    }
+
     togglePanelOpen() {
         this.setState({open: !this.state.open});
     }
@@ -90,7 +101,7 @@ class CatalogBrowser extends React.Component {
 
     render() {
 
-        let tabs = [];
+         let tabs = [];
         for (let primaryType in Category) {
             if (Category.hasOwnProperty(primaryType)) {
                 tabs.push(
@@ -129,13 +140,13 @@ class CatalogBrowser extends React.Component {
                             </small>
                         </h4>
                     </div>
-                    <div className="panel-body" >
+                    <div className="panel-body">
                         <Tabs id="primaryTabs" onSelect={this.onTabSelect}>
                             <Tab eventKey={"all"} title={"all"}/>
                             {tabs}
                             <Tab eventKey={"other"} title={"other"}/>
                         </Tabs>
-                        <TemplateFilterPanel open={this.state.open}
+                        <TemplateFilterPanel open={this.state.open} onItemClick={this.onItemClick}
                                              items={this.items} filter={this.state.filter}/>
                     </div>
                 </div>
@@ -187,18 +198,6 @@ class PrimaryTab extends React.Component {
 
 class SecondaryThumbnail extends React.Component {
     render() {
-        {/*<a onClick={() => {*/
-        }
-        {/*this.props.onClick(this.props.secondaryType)*/
-        }
-        {/*}}><Image height={20}*/
-        }
-        {/*src={this.props.iconUrl ? "img/" + this.props.iconUrl : "/img/logo/faclone.svg"}/>*/
-        }
-        {/*{this.props.title}*/
-        }
-        {/*</a>*/
-        }
         return (
             <a style={{cursor: "pointer"}} className="text-center" onClick={() => {
                 this.props.onClick(this.props.secondaryType)
@@ -231,22 +230,55 @@ class TemplateFilterPanel extends React.Component {
 
         for (let i = 0; i < this.filteredItems.length; i++) {
             items.push(
-                <div key={this.filteredItems[i].data.metadata.name}>
-                    <h4>{this.filteredItems[i].displayName}</h4>
-                    <p>{this.filteredItems[i].primaryType}</p>
-                </div>
+                <Col key={this.filteredItems[i].data.metadata.name} md={3}>
+                    <ItemView
+                        item={this.filteredItems[i]}
+                        onClick={() => {
+                            this.props.onItemClick(this.filteredItems[i])
+                        }}/>
+                </Col>
             )
         }
 
         return (
             <Collapse in={this.props.open}>
-                <div>
+                <Row>
                     {items}
-                </div>
+                </Row>
             </Collapse>
         )
 
     }
+}
+
+class ItemView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let item = this.props.item;
+        return (
+            <div className="text-center" style={{cursor: "pointer", maxHeight: 150, minHeight: 150}}
+                 onClick={this.props.onClick}>
+                <div className="center-block" style={{
+                    height: 80,
+                    width: 80,
+                    border: "2px solid grey",
+                    borderTopRightRadius: "50%",
+                    borderTopLeftRadius: "50%",
+                    borderBottomRightRadius: "50%",
+                    borderBottomLeftRadius: "50%"
+                }}>
+                    <img className="center-block" style={{position: "relative", top: "50%", transform: "translateY(-50%)"}} src={item.iconClass ?
+                        "img/" + findIconClass(item.iconClass) : "img/logo/faclone.svg"}
+                         width={50}/>
+                </div>
+                <strong>{item.displayName}</strong>
+            </div>
+        )
+    }
+
 }
 
 
--- app/components/HomePage/CatalogBrowser.run.js
+++ app/components/HomePage/CatalogBrowser.run.js
@@ -1,34 +1,6 @@
 import {Filter, Item} from "./CatalogBrowser";
+import {Category} from "./constants";
 
-/**
- * tag-primaryType conversion map
- * @type {{java: string}}
- */
-export const Category = {
-    'language':
-        {
-            'java': {iconUrl: 'logo/openjdk.svg', tags: ['java']},
-            'dotnet': {iconUrl: 'logo/dotnet.svg', tags: ['dotnet', '.net']},
-            'javascript': {iconUrl: 'logo/js.svg', tags: ['nodejs']},
-            'perl': {iconUrl: 'logo/perl.svg', tags: ['perl']},
-            'ruby': {iconUrl: 'logo/ruby.svg', tags: ['ruby']},
-            'php': {iconUrl: 'logo/php.svg', tags: ['php']},
-            'python': {iconUrl: 'logo/python.svg', tags: ['python']}
-        },
-    'database':
-        {
-            'mongo': {iconUrl: 'logo/mongodb.svg', tags: ['mongodb']},
-            'mysql': {iconUrl: 'logo/mysql-database.svg', tags: ['mysql']},
-            'postgres': {iconUrl: 'logo/postgresql.svg', tags: ['postgresql']},
-            'mariadb': {iconUrl: 'logo/mariadb.svg', tags: ['mariadb']},
-        },
-    'middleware': {
-        'runtime': {tags: ['httpd']}
-    },
-    'cicd': {
-        'jenkins': {iconUrl: 'logo/jenkins.svg', tags: ['jenkins']}
-    }
-};
 
 let TagMap = (function () {
     let result = {};
