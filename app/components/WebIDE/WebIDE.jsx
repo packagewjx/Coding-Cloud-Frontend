5de08359d0bce2bc3b9b2724d75f58d9496d2816
Wujunxian <wu812730157@gmail.com>
2018-02-07 09:57:18 +0800

    added leave webide prompt

--- app/components/WebIDE/WebIDE.jsx
+++ app/components/WebIDE/WebIDE.jsx
@@ -4,10 +4,26 @@
  Description: WebIDE
  */
 
+import {withRouter} from "react-router";
+
 var React = require('react');
 class WebIDE extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.routerWillLeave = this.routerWillLeave.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.router.setRouteLeaveHook(this.props.route, this.routerWillLeave);
+    }
+
+    routerWillLeave() {
+        return "你确定要离开WebIDE吗？";
+    }
 
     render() {
+        console.log(this);
         return (
             <iframe src="http://116.56.140.108:8080/" frameBorder="0" width="100%" height="1000">/*WebIDE嵌入*/
             </iframe>
@@ -16,4 +32,4 @@ class WebIDE extends React.Component {
 
 }
 
-export default WebIDE;
+export default withRouter(WebIDE);
